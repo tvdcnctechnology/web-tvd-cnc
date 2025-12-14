4936bc4d5de0412b3e9d380f64e2ad68975055eb
tvdcnctechnology <tvdcnctechnology@gmail.com>
2025-12-14 22:09:23 +0700

    add aboutus
    add aboutus

--- assets/js/tvd.js
+++ assets/js/tvd.js
@@ -10,5 +10,17 @@
       })
       .catch(error => console.error("Error fetching data:", error));
 
+ fetch("data/tvd-aboutus.json")
+    .then(res => res.json())
+      .then(data => {
+        // Loop through the array of key/value pairs
+        data.forEach(item => {
+          // Use class selector so multiple elements can be updated
+          const elements = document.querySelectorAll(`.${item.key}`);
+          elements.forEach(el => el.innerHTML = item.value);
+        });
+      })
+      .catch(error => console.error("Error fetching data:", error));
+
 
       
