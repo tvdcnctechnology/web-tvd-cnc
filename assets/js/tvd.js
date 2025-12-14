48de1f2f484852664b981ec8df39c1c1339040f3
tvdcnctechnology <tvdcnctechnology@gmail.com>
2025-12-14 22:38:48 +0700

    add expert
    add expert

--- assets/js/tvd.js
+++ assets/js/tvd.js
@@ -41,5 +41,24 @@
       })
       .catch(error => console.error("Error fetching data:", error));
 
+ fetch("data/tvd-expert.json")
+     .then(res => res.json())
+      .then(data => {
+        data.forEach(item => {
+          const elements = document.querySelectorAll(`.${item.key}`);
+          
+          elements.forEach(el => {
+            // If the key ends with "_url", set src attribute
+            if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
+              el.src = item.value;
+            } else {
+              // Otherwise, update innerHTML
+              el.innerHTML = item.value;
+            }
+          });
+        });
+      })
+      .catch(error => console.error("Error fetching data:", error));
+
 
       
