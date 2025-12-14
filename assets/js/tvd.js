fbe029fd328917759df43c1ca7b2e7e7e3ffeaa5
tvdcnctechnology <tvdcnctechnology@gmail.com>
2025-12-14 21:41:25 +0700

    update title
    update

--- assets/js/tvd.js
+++ assets/js/tvd.js
@@ -1,1 +1,13 @@
-xxx
+ fetch("data/tvd-home.json")
+  .then(res => res.json())
+  .then(data => {
+    const info = data[0];
+    for (const key in info) {
+      const elements = document.querySelectorAll(`.${key}`); // select by class
+      elements.forEach(el => el.innerHTML = info[key]);     // update all
+    }
+  })
+  .catch(err => console.error(err));
+
+
+      
