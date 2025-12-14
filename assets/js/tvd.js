3d185482de29d167db4a8d52ade500b4e9f89565
tvdcnctechnology <tvdcnctechnology@gmail.com>
2025-12-15 00:43:33 +0700

    update data
    update data

--- assets/js/tvd.js
+++ assets/js/tvd.js
@@ -60,5 +60,23 @@
       })
       .catch(error => console.error("Error fetching data:", error));
 
+ fetch("data/tvd-information.json")
+     .then(res => res.json())
+      .then(data => {
+        data.forEach(item => {
+          const elements = document.querySelectorAll(`.${item.key}`);
+          
+          elements.forEach(el => {
+            // If the key ends with "_url", set src attribute
+            if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
+              el.src = item.value;
+            } else {
+              // Otherwise, update innerHTML
+              el.innerHTML = item.value;
+            }
+          });
+        });
+      })
+      .catch(error => console.error("Error fetching data:", error));
 
       
