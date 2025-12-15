550298878b1a76c2e12e0e86d11b2835effa7413
tvdcnctechnology <tvdcnctechnology@gmail.com>
2025-12-15 23:00:13 +0700

    ีupdate
    ีupdate

--- assets/js/tvd.js
+++ assets/js/tvd.js
@@ -1,114 +1,114 @@
- fetch("data/tvd-home.json")
-    .then(res => res.json())
-      .then(data => {
-        // Loop through the array of key/value pairs
-        data.forEach(item => {
-          // Use class selector so multiple elements can be updated
-          const elements = document.querySelectorAll(`.${item.key}`);
-          elements.forEach(el => el.innerHTML = item.value);
-        });
-      })
-      .catch(error => console.error("Error fetching data:", error));
+fetch("data/tvd-home.json")
+  .then(res => res.json())
+  .then(data => {
+    // Loop through the array of key/value pairs
+    data.forEach(item => {
+      // Use class selector so multiple elements can be updated
+      const elements = document.querySelectorAll(`.${item.key}`);
+      elements.forEach(el => el.innerHTML = item.value);
+    });
+  })
+  .catch(error => console.error("Error fetching data:", error));
 
- fetch("data/tvd-aboutus.json")
-    .then(res => res.json())
-      .then(data => {
-        // Loop through the array of key/value pairs
-        data.forEach(item => {
-          // Use class selector so multiple elements can be updated
-          const elements = document.querySelectorAll(`.${item.key}`);
-          elements.forEach(el => el.innerHTML = item.value);
-        });
-      })
-      .catch(error => console.error("Error fetching data:", error));
+fetch("data/tvd-aboutus.json")
+  .then(res => res.json())
+  .then(data => {
+    // Loop through the array of key/value pairs
+    data.forEach(item => {
+      // Use class selector so multiple elements can be updated
+      const elements = document.querySelectorAll(`.${item.key}`);
+      elements.forEach(el => el.innerHTML = item.value);
+    });
+  })
+  .catch(error => console.error("Error fetching data:", error));
 
- fetch("data/tvd-services.json")
-     .then(res => res.json())
-      .then(data => {
-        data.forEach(item => {
-          const elements = document.querySelectorAll(`.${item.key}`);
-          
-          elements.forEach(el => {
-            // If the key ends with "_url", set src attribute
-            if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
-              el.src = item.value;
-            } else {
-              // Otherwise, update innerHTML
-              el.innerHTML = item.value;
-            }
-          });
-        });
-      })
-      .catch(error => console.error("Error fetching data:", error));
+fetch("data/tvd-services.json")
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(item => {
+      const elements = document.querySelectorAll(`.${item.key}`);
 
- fetch("data/tvd-expert.json")
-     .then(res => res.json())
-      .then(data => {
-        data.forEach(item => {
-          const elements = document.querySelectorAll(`.${item.key}`);
-          
-          elements.forEach(el => {
-            // If the key ends with "_url", set src attribute
-            if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
-              el.src = item.value;
-            } else {
-              // Otherwise, update innerHTML
-              el.innerHTML = item.value;
-            }
-          });
-        });
-      })
-      .catch(error => console.error("Error fetching data:", error));
+      elements.forEach(el => {
+        // If the key ends with "_url", set src attribute
+        if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
+          el.src = item.value;
+        } else {
+          // Otherwise, update innerHTML
+          el.innerHTML = item.value;
+        }
+      });
+    });
+  })
+  .catch(error => console.error("Error fetching data:", error));
 
- fetch("data/tvd-information.json")
-     .then(res => res.json())
-      .then(data => {
-        data.forEach(item => {
-          const elements = document.querySelectorAll(`.${item.key}`);
-          
-          elements.forEach(el => {
-            // If the key ends with "_url", set src attribute
-            if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
-              el.src = item.value;
-            } else {
-              // Otherwise, update innerHTML
-              el.innerHTML = item.value;
-            }
-          });
-        });
-      })
-      .catch(error => console.error("Error fetching data:", error));
+fetch("data/tvd-expert.json")
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(item => {
+      const elements = document.querySelectorAll(`.${item.key}`);
+
+      elements.forEach(el => {
+        // If the key ends with "_url", set src attribute
+        if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
+          el.src = item.value;
+        } else {
+          // Otherwise, update innerHTML
+          el.innerHTML = item.value;
+        }
+      });
+    });
+  })
+  .catch(error => console.error("Error fetching data:", error));
+
+fetch("data/tvd-information.json")
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(item => {
+      const elements = document.querySelectorAll(`.${item.key}`);
+
+      elements.forEach(el => {
+        // If the key ends with "_url", set src attribute
+        if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
+          el.src = item.value;
+        } else {
+          // Otherwise, update innerHTML
+          el.innerHTML = item.value;
+        }
+      });
+    });
+  })
+  .catch(error => console.error("Error fetching data:", error));
 
 fetch("data/tvd-contact.json")
-     .then(res => res.json())
-      .then(data => {
-        data.forEach(item => {
-          const elements = document.querySelectorAll(`.${item.key}`);
-          
-          elements.forEach(el => {
-            // If the key ends with "_url", set src attribute
-            if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
-              el.src = item.value;
-            }
-            else if (item.key.endsWith("_map") && el.tagName.toLowerCase() === "iframe") {
-              el.src = item.value;
-            }
-            else if (item.key.endsWith("_web") && el.tagName.toLowerCase() === "a") {
-              el.href = item.value;
-              el.innerHTML = item.value;
-            }
-            else if (item.key.endsWith("_fb") && el.tagName.toLowerCase() === "a") {
-              el.href = item.value;
-            }
-            else if (item.key.endsWith("_mail") && el.tagName.toLowerCase() === "a") {
-              el.href = "mailto:"+ item.value;
-              el.innerHTML = item.value;
-            }
-             else {
-              // Otherwise, update innerHTML
-              el.innerHTML = item.value;
-            }
-          });
-        });
-      })
-      .catch(error => console.error("Error fetching data:", error));
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(item => {
+      const elements = document.querySelectorAll(`.${item.key}`);
+
+      elements.forEach(el => {
+        // If the key ends with "_url", set src attribute
+        if (item.key.endsWith("_url") && el.tagName.toLowerCase() === "img") {
+          el.src = item.value;
+        }
+        else if (item.key.endsWith("_map") && el.tagName.toLowerCase() === "iframe") {
+          el.src = item.value;
+        }
+        else if (item.key.endsWith("_web") && el.tagName.toLowerCase() === "a") {
+          el.href = item.value;
+          el.innerHTML = item.value;
+        }
+        else if (item.key.endsWith("_fb") && el.tagName.toLowerCase() === "a") {
+          el.href = item.value;
+        }
+        else if (item.key.endsWith("_mail") && el.tagName.toLowerCase() === "a") {
+          el.href = "mailto:" + item.value;
+          el.innerHTML = item.value;
+        }
+        else {
+          // Otherwise, update innerHTML
+          el.innerHTML = item.value;
+        }
+      });
+    });
+  })
+  .catch(error => console.error("Error fetching data:", error));
