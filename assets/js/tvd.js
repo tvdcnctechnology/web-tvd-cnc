9cd4fe48298254b48d4c77bf70e7d53e63cb87da
tvdcnctechnology <tvdcnctechnology@gmail.com>
2026-01-20 19:32:51 +0700

    ud

--- assets/js/tvd.js
+++ assets/js/tvd.js
@@ -1,11 +1,23 @@
-fetch("data/tvd-home.json")
+
+
+fetch("/data/tvd-home.json")
   .then(res => res.json())
   .then(data => {
     // Loop through the array of key/value pairs
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const selectedType = urlParams.get('type');
+
     data.forEach(item => {
       // Use class selector so multiple elements can be updated
       const elements = document.querySelectorAll(`.${item.key}`);
       elements.forEach(el => el.innerHTML = item.value);
+
+     if (selectedType && item.key == "menu_type"+selectedType){
+        const eleProductTitle = document.querySelectorAll(`.${"product_title"}`);
+        eleProductTitle.forEach(el => el.innerHTML = item.value);
+      }
+
     });
   })
   .catch(error => console.error("Error fetching data:", error));
